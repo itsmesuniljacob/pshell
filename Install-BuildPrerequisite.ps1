181064cc19169677f431620f056c47d31a0ffbc9
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 14:23:57 +0530

    Administrator build

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -14,7 +14,7 @@
 
 #Requires -RunAsAdministrator
 #Or
-# 
+#
 # if (([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))
 # {
 #   "Running as admin in $PSScriptRoot"
@@ -27,7 +27,19 @@
 # }
 #
 # "Doing this only as admin."
-
+Write-Host "Checking for elevation... "
+$CurrentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+if (($CurrentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)) -eq $false)
+{
+	$ArgumentList = "-noprofile -noexit -file `"{0}`" -Path `"$Path`" -MaxStage $MaxStage"
+	If ($ValidateOnly) { $ArgumentList = $ArgumentList + " -ValidateOnly" }
+	If ($SkipValidation) { $ArgumentList = $ArgumentList + " -SkipValidation $SkipValidation" }
+	If ($Mode) { $ArgumentList = $ArgumentList + " -Mode $Mode" }
+	Write-Host "elevating"
+	Start-Process powershell.exe -Verb RunAs -ArgumentList ($ArgumentList -f ($myinvocation.MyCommand.Definition)) -Wait
+	Exit
+}
+write-host "in admin mode.."
 #Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 - Scope CurrentUser -Force
 Install-Module -Name "PSScriptAnalyzer" -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name "Pester" -Scope CurrentUser -Force -SkipPublisherCheck
