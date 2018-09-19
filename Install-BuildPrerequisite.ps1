e44db6700341962e8a9abb3c60ef7f9c4a842fe1
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 11:47:57 +0530

    Administrator build

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -1,10 +1,18 @@
+param([switch]$Elevated)
 $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
 $testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
-$testadmin
-if ($testadmin -eq $false) {
-Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
-exit $LASTEXITCODE
+
+function Test-Admin {
+  $currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+  $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
 }
+
+if ($testadmin -eq $false) {
+  Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+  exit $LASTEXITCODE
+}
+
+'running with full privileges'
 
 Install-PackageProvider -Name "NuGet" -Force
 Install-Module -Name "PSScriptAnalyzer" -Force -SkipPublisherCheck
