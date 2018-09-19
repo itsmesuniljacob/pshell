b21a46973929078e33d732b33f9d7480ad3cc003
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 12:41:27 +0530

    Administrator build

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -1,4 +1,4 @@
 #Install-PackageProvider -Name NuGet -Force
-Install-Module -Name "NuGet" -Scope CurrentUser -Force -SkipPublisherCheck
+#Install-Module -Name "NuGet" -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name "PSScriptAnalyzer" -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name "Pester" -Scope CurrentUser -Force -SkipPublisherCheck
