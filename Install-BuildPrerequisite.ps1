9f70ef9906f834adc86fe6f25737ff5722d04942
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 11:38:04 +0530

    Administrator build

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -1,8 +1,9 @@
-function Confirm-AdministratorContext
-{
-    $administrator = [Security.Principal.WindowsBuiltInRole] "Administrator"
-    $identity = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
-    $identity.IsInRole($administrator)
+$currentUser = New-Object Security.Principal.WindowsPrincipal $([Security.Principal.WindowsIdentity]::GetCurrent())
+$testadmin = $currentUser.IsInRole([Security.Principal.WindowsBuiltinRole]::Administrator)
+$testadmin
+if ($testadmin -eq $false) {
+Start-Process powershell.exe -Verb RunAs -ArgumentList ('-noprofile -noexit -file "{0}" -elevated' -f ($myinvocation.MyCommand.Definition))
+exit $LASTEXITCODE
 }
 
 Install-PackageProvider -Name "NuGet" -Force
