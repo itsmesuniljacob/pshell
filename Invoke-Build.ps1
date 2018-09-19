bb30ce0acd048c6f58352b8e440843697fd69aae
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 19:43:19 +0530

    Appveyor config- Admin -1

--- Invoke-Build.ps1
+++ Invoke-Build.ps1
@@ -1,62 +1,54 @@
 param([switch]$AppVeyor=$false)
- $ErrorActionPreference="Stop"
+$ErrorActionPreference="Stop"
 function Confirm-AdministratorContext
 {
     $administrator = [Security.Principal.WindowsBuiltInRole] "Administrator"
     $identity = [Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()
     $identity.IsInRole($administrator)
 }
- function Use-AdministratorContext
-{
-    if (-not (Confirm-AdministratorContext))
-    {
-        $arguments = "& '" + $myinvocation.mycommand.definition + "'"
-        Start-Process powershell -Verb runAs -ArgumentList $arguments
-        Break
-    }
-}
- function Install-BuildPrerequisite
-{
-    foreach ($arg in $args)
-    {
-        Write-Output "Checking build prerequisite module $arg"
-        if (-not (Get-Module -Name $arg))
-        {
-            Write-Output "Requesting elevated permissions to install build prerequisite module $arg"
-            Use-AdministratorContext
-            Write-Output "Installing build prerequisite module $arg"
-            Install-Module -Name $arg -Force -SkipPublisherCheck
-        }
-        else
-        {
-            Write-Output "Confirmed already installed build prerequisite module $arg"
-        }
-    }
-}
- function Invoke-StaticAnalysis
+
+function Invoke-StaticAnalysis
 {
     $result = Invoke-ScriptAnalyzer -Path "." -Recurse
-@@ -44,14 +15,11 @@ function Invoke-StaticAnalysis
+    $result
+    if ($result.Length -gt 0)
     {
         throw "Build failed. Found $($result.Length) static analysis issue(s)"
-    }
-    else
-    {
-        Write-Output "Static analysis findings clean"
     }
     Write-Output "Static analysis findings clean"
 }
 function Invoke-Build
 {
     if ($AppVeyor)
-    if ($AppVeyor)
     {
         Write-Output "Building in AppVeyor build CI server context"
     }
-@@ -80,7 +48,41 @@ function Invoke-Test
+    else
+    {
+        Write-Output "Building in normal context"
+    }
+    Write-Output "Checking static analysis findings"
+    Invoke-StaticAnalysis
+    Write-Output "Running tests"
+    Invoke-Test
+}
+
+function Send-TestResult([string]$resultsPath)
+{
+    $url = "https://ci.AppVeyor.com/api/testresults/nunit/$($env:AppVeyor_JOB_ID)"
+    (New-Object 'System.Net.WebClient').UploadFile($url, (Resolve-Path $resultsPath))
+}
+function Invoke-Test
+{
+    $resultsPath = ".\TestResults.xml"
+    Invoke-Pester -EnableExit -OutputFormat NUnitXml -OutputFile $resultsPath
+    if ($AppVeyor)
+    {
+        Send-TestResult $resultsPath
     }
 }
- function Confirm-Prerequisite
+
+function Confirm-Prerequisite
 {
     (
         (Get-Module -Name PSScriptAnalyzer).Length *
@@ -64,13 +56,15 @@ function Invoke-Build
         (Get-PackageProvider -Name Nuget).Length
     ) -ne 0
 }
- function Install-Prerequisite
+
+function Install-Prerequisite
 {
     if ((Confirm-Prerequisite))
     {
         return
     }
-     Write-Output "Installing build prerequisites"
+
+    Write-Output "Installing build prerequisites"
     $code = ".\Install-BuildPrerequisites.ps1"
     if (Confirm-AdministratorContext)
     {
@@ -80,14 +74,15 @@ function Invoke-Build
     {
         Start-Process -FilePath powershell.exe -ArgumentList $code -verb RunAs
     }
-     if (-not (Confirm-Prerequisite))
+
+    if (-not (Confirm-Prerequisite))
     {
         throw "Build Failed. Installation of build prerequisites failed."
     }
 }
- Write-Output "Build starting"
-Install-BuildPrerequisite "PSScriptAnalyzer" "Pester"
+
+Write-Output "Build starting"
 #Install-Prerequisite
 Write-Output "Building"
 Invoke-Build
-Write-Output "Build complete" 
+Write-Output "Build complete"
