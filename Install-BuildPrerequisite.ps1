7e7198625c03ef98023eb4f2fad02e97b4bbfc26
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 12:13:24 +0530

    Administrator build

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -28,6 +28,6 @@ else
 
 "Doing this only as admin."
 
-Install-PackageProvider -Name "NuGet" -Force
+Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 - Scope CurrentUser -Force
 Install-Module -Name "PSScriptAnalyzer" -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name "Pester" -Scope CurrentUser -Force -SkipPublisherCheck
