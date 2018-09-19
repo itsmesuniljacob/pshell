035802e1da3998d94db40b61c6015d9fcbee9e5b
Sunil.Jacob <sunil1405@gmail.com>
2018-09-19 12:20:52 +0530

    Administrator build

--- Install-BuildPrerequisite.ps1
+++ Install-BuildPrerequisite.ps1
@@ -1,3 +1,4 @@
+Register-PSRepository -Name "PSGallery" –SourceLocation "https://www.powershellgallery.com/api/v2/" -InstallationPolicy Trusted
 Install-PackageProvider -Name NuGet -RequiredVersion 2.8.5.201 -Scope CurrentUser -Force
 Install-Module -Name "PSScriptAnalyzer" -Scope CurrentUser -Force -SkipPublisherCheck
 Install-Module -Name "Pester" -Scope CurrentUser -Force -SkipPublisherCheck
